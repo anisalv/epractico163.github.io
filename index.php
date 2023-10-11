d304582586b7f04350331633050171c538034d20
anisalv <anaa06119@gmail.com>
2023-10-11 08:10:00 -0400

    Updated

---
+++ index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BCBConnect</title>
+    <style>
+        body {
+        font-family: 'Poppins', sans-serif;
+        color: #0c0c0c;
+        background-color: #ffffff;
+        }
+
+        header {
+            background-color: #7395D1;
+            color: #132043;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            font-size: 36px;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #0073e6;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            font-weight: bold;
+            margin-right: 10px;
+        }
+
+        .btn:hover {
+            background-color: #0051a8;
+        }
+
+    </style>
+</head>
+<body>
+    <header>
+        <h1>BCBConnect</h1>
+    </header>
+    <div class="container">
+        <h2>Bienvenido a nuestro sistema bancario</h2>
+        <p>Por favor, seleccione una opción:</p>
+        <a href="login.php" class="btn">Iniciar Sesión</a>
+        <a href="registro.php" class="btn">Registrarse</a>
+        
+       </div>
